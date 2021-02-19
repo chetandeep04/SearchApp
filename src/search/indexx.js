608732b8159a8f068a182f8446b5d9c4d7d3cda2
chetandeep04 <chetandeep1232.cse18@chitkara.edu.in>
2021-02-19 23:23:46 +0530

    setState done

--- src/search/indexx.js
+++ src/search/indexx.js
@@ -1,20 +1,19 @@
 import React,{Component} from 'react';
-import List from './list.json';
 import './style.css';
 export default class Main extends React.Component{
-    render(){
-        return (
-        <>
-        <div className="main">
-            <input type="text" id="searchbar" placeholder="Search Your Employees"/>
-            <table border="1">
-                <tr><th>Name</th><th>Location</th></tr>
-            {List.map((employee,index)=>{
-            return <tr><td>{employee.name}</td><td>{employee.location}</td></tr>
-        }
-            )}</table>
-        </div>
-        </>
-        )
+    state={
+        value:'',
     }
+    Search=(event)=>{
+      this.setState({
+          value:event.target.value,
+      })
+    }
+render()
+{
+    return <div className="main">
+       <input placeholder="Search ....." onChange={this.Search}></input>
+    </div>
+
+}
 }
