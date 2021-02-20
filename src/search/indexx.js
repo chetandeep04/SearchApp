3a745a22de612a4536708318e212d29bba505fa3
chetandeep04 <chetandeep1232.cse18@chitkara.edu.in>
2021-02-20 22:17:23 +0530

    app completed

--- src/search/indexx.js
+++ src/search/indexx.js
@@ -1,19 +1,26 @@
-import React,{Component} from 'react';
+import React,{Component, useState} from 'react';
 import './style.css';
-export default class Main extends React.Component{
-    state={
-        value:'',
+import List from './list.json';
+export default function Main(){
+    const [searchTerm, setSearchTerm]=useState("");
+      return <div className="main">
+       <input placeholder="Enter Employee Name ....." onChange={(event)=>{
+           setSearchTerm(event.target.value);
+       }}></input>
+    <table>
+{List.filter((val)=>{
+    if(searchTerm==''){
+        return val;
     }
-    Search=(event)=>{
-      this.setState({
-          value:event.target.value,
-      })
+    else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val;
     }
-render()
-{
-    return <div className="main">
-       <input placeholder="Search ....." onChange={this.Search}></input>
-    </div>
+}).map((val,key)=>{
+    return <tr><td>{val.name}</td><td>{val.location}</td></tr>
+    
+})}
+</table>
+</div>
+    
 
-}
 }
